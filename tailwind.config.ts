df26963dd3301d481ea3efcb46da5e6dee3ab9f2
[ISW] <is@werhiz.com>
2025-12-07 10:45:37 -0500

    chore: initial project setup and config

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import forms from "@tailwindcss/forms";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@rewind-ui/core/dist/theme/styles/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    typography,
+    forms,
+  ],
+};
+export default config;
